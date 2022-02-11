a42e255b419018a07fadc456bb0689ae12e66ac8
fatima-marriam <fatimamarriam842@gmail.com>
2022-02-11 22:46:01 +0500

    update services

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+//=====================nav bar effect on scroll 
 window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 } );
+
+// =======================service section - modal
+ const serviceModals = document.querySelectorAll('.service-modal');
+ const learnmoreBtns = document.querySelectorAll('.learn-more-btn');
+ const modalCloseBtns = document.querySelectorAll('.modal-close-btn');
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+learnmoreBtns.forEach((lmb, i)=>{
+    lmb.addEventListener('click' , ()=>{
+        modal(i);
+    });
+});
+modalCloseBtns.forEach((mcb)=>{
+mcb.addEventListener('click', ()=>{
+    serviceModals.forEach((mv) => {
+        mv.classList.remove("active");
+    });
+});
+});
