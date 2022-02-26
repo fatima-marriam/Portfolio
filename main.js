c3339f970c4312ae25424071d472317f47558d93
fatima-marriam <fatimamarriam842@gmail.com>
2022-02-26 16:10:19 +0500

    Update Portfolio Section

--- main.js
+++ main.js
@@ -23,3 +23,22 @@ mcb.addEventListener('click', ()=>{
     });
 });
 });
+//portfolio section model 
+const portfolioModals = document.querySelectorAll('.portfolio-model');
+ const imgCards = document.querySelectorAll('.img-card');
+ const portfolioCloseBtns = document.querySelectorAll('.portfolio-close-btn');
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+imgCards.forEach((imgCard, i)=>{
+    imgCard.addEventListener('click' , ()=>{
+        portfolioModal(i);
+    });
+});
+portfolioCloseBtns.forEach((portfolioCloseBtn)=>{
+    portfolioCloseBtn.addEventListener('click', ()=>{
+    portfolioModals.forEach((portfolioModalView) => {
+        portfolioModalView.classList.remove("active");
+    });
+});
+});
