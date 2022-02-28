5a3a83b68405054177a3c503ec41cd07b4f1741c
fatima-marriam <fatimamarriam842@gmail.com>
2022-02-28 15:41:01 +0500

    scroll top btn

--- main.js
+++ main.js
@@ -56,3 +56,12 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+//   Scroll to top button
+ const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+ window.addEventListener("scroll",function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500); 
+});
+scrollTopBtn.addEventListener("click", ()=>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
