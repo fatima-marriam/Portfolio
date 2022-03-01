528cfca73760c4df0e76a7121c376eecdbe3499d
fatima-marriam <fatimamarriam842@gmail.com>
2022-03-01 23:22:00 +0500

    add Query

--- main.js
+++ main.js
@@ -100,3 +100,20 @@ window.addEventListener("scroll", () => {
      document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
      themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
  }
+//  ====Responsive navigation menu toggle==============================================
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", ()=> {
+    navigation.classList.add("active");
+});
+closeBtn.addEventListener("click", ()=> {
+    navigation.classList.remove("active");
+});
+navItems.forEach((navItem)=> {
+    navItem.addEventListener("click", ()=> {
+        navigation.classList.remove("active");
+    });
+});
